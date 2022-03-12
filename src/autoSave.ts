ac2ac8fa55e9c0ad51d68dabc7639ee897041abb
Luiz Ferraz <luiz@lferraz.com>
2022-03-12 08:58:27 -0300

    Implement AutoSaveCache (#4)

---
+++ src/autoSave.ts
@@ -0,0 +1,30 @@
+import {CacheLoader, CacheProvider} from './cacheProvider';
+
+/**
+ * A cache provider that automatically caches loaded values.
+ */
+export class AutoSaveCache<K, V> implements CacheProvider<K, V> {
+    private readonly cacheProvider: CacheProvider<K, V>;
+
+    public constructor(cacheProvider: CacheProvider<K, V>) {
+        this.cacheProvider = cacheProvider;
+    }
+
+    public get(key: K, loader: CacheLoader<K, V>): Promise<V> {
+        return this.cacheProvider.get(key, async () => {
+            const value = await loader(key);
+
+            await this.cacheProvider.set(key, value);
+
+            return value;
+        });
+    }
+
+    public set(key: K, value: V): Promise<void> {
+        return this.cacheProvider.set(key, value);
+    }
+
+    public delete(key: K): Promise<void> {
+        return this.cacheProvider.delete(key);
+    }
+}
