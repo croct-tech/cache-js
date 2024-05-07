214fcb8e765aa2966360245089351bacc5ef6908
Luiz Ferraz <luiz.ferraz@croct.com>
2024-05-07 18:45:37 -0300

    Update

--- src/defaultWhileMiss.ts
+++ src/defaultWhileMiss.ts
@@ -2,7 +2,6 @@ import {CacheLoader, CacheProvider} from './cacheProvider';
 
 type Configuration<K, V> = {
     cacheProvider: CacheProvider<K, V>,
-    timeout: number,
     defaultValue: V,
 
     /**
@@ -14,34 +13,24 @@ type Configuration<K, V> = {
 export class DefaultWhileMissCache<K, V> implements CacheProvider<K, V> {
     private readonly cacheProvider: Configuration<K, V>['cacheProvider'];
 
-    private readonly timeout: number;
-
     private readonly defaultValue: V;
 
     private readonly errorHandler: (error: Error) => void;
 
     public constructor(config: Configuration<K, V>) {
         this.cacheProvider = config.cacheProvider;
-        this.timeout = config.timeout;
         this.defaultValue = config.defaultValue;
         this.errorHandler = config.errorHandler ?? ((): void => { /* noop */ });
     }
 
     public get(key: K, loader: CacheLoader<K, V>): Promise<V> {
-        const resolveWithTimeout = (value: V): Promise<V> => new Promise(resolve => {
-            setTimeout(() => resolve(value), this.timeout);
+        return this.cacheProvider.get(key, innerKey => {
+            loader(innerKey).catch(
+                error => this.errorHandler(error),
+            );
+
+            return Promise.resolve(this.defaultValue);
         });
-
-        return Promise.race([
-            resolveWithTimeout(this.defaultValue),
-            this.cacheProvider
-                .get(key, loader)
-                .catch(error => {
-                    this.errorHandler(error);
-
-                    return this.defaultValue;
-                }),
-        ]);
     }
 
     public set(key: K, value: V): Promise<void> {
