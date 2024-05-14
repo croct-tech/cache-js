4d13f20c63c0af094b4088ca8533c3dd95696ff4
Renan <renanoliveira628@gmail.com>
2024-05-14 15:45:02 -0300

    apply requested changes
    [skip ci]

--- src/defaultWhileMiss.ts
+++ src/defaultWhileMiss.ts
@@ -1,7 +1,7 @@
 import {CacheLoader, CacheProvider} from './cacheProvider';
 
 export type Configuration<K, V> = {
-    cacheProvider: CacheProvider<K, V>,
+    provider: CacheProvider<K, V>,
     defaultValue: V,
 
     /**
@@ -11,33 +11,31 @@ export type Configuration<K, V> = {
 };
 
 export class DefaultWhileMissCache<K, V> implements CacheProvider<K, V> {
-    private readonly cacheProvider: Configuration<K, V>['cacheProvider'];
+    private readonly provider: Configuration<K, V>['provider'];
 
     private readonly defaultValue: V;
 
     private readonly errorHandler: (error: Error) => void;
 
     public constructor(config: Configuration<K, V>) {
-        this.cacheProvider = config.cacheProvider;
+        this.provider = config.provider;
         this.defaultValue = config.defaultValue;
         this.errorHandler = config.errorHandler ?? ((): void => { /* noop */ });
     }
 
     public get(key: K, loader: CacheLoader<K, V>): Promise<V> {
-        return this.cacheProvider.get(key, innerKey => {
-            loader(innerKey).catch(
-                error => this.errorHandler(error),
-            );
+        return this.provider.get(key, innerKey => {
+            loader(innerKey).catch(this.errorHandler);
 
             return Promise.resolve(this.defaultValue);
         });
     }
 
     public set(key: K, value: V): Promise<void> {
-        return this.cacheProvider.set(key, value);
+        return this.provider.set(key, value);
     }
 
     public delete(key: K): Promise<void> {
-        return this.cacheProvider.delete(key);
+        return this.provider.delete(key);
     }
 }
--- test/defaultWhileMiss.test.ts
+++ test/defaultWhileMiss.test.ts
@@ -1,85 +1,79 @@
-import {extractErrorMessage, InMemoryLogger, LogLevel} from '@croct/logging';
-import {NoopCache} from '../src';
-import {DefaultWhileMissCache} from '../src/defaultWhileMiss';
+import {DefaultWhileMissCache, NoopCache} from '../src';
 
-describe('A cache provider that returns a default value while load on the background', () => {
+describe('A cache provider that returns a default value while loading in the background', () => {
     it('should return a default value calling the loader the background', async () => {
-        const inner = new NoopCache();
+        const provider = new NoopCache();
 
-        jest.spyOn(inner, 'get');
+        jest.spyOn(provider, 'get');
 
         const loader = jest.fn().mockResolvedValue('loaderValue');
 
         const cache = new DefaultWhileMissCache({
-            cacheProvider: inner,
+            provider: provider,
             defaultValue: 'defaultValue',
         });
 
         await expect(cache.get('key', loader)).resolves.toBe('defaultValue');
 
-        expect(inner.get).toHaveBeenCalledWith('key', expect.any(Function));
+        expect(provider.get).toHaveBeenCalledWith('key', expect.any(Function));
 
         expect(loader).toHaveBeenCalledWith('key');
     });
 
-    it('should return a default value handling a background loading error', async () => {
-        const inner = new NoopCache();
-        const logger = new InMemoryLogger();
+    it('should return a default value handling a loading error in background', async () => {
+        const provider = new NoopCache();
 
-        jest.spyOn(inner, 'get');
+        jest.spyOn(provider, 'get');
 
-        const loader = jest.fn().mockRejectedValue(new Error('Some error.'));
+        const error = new Error('Some error.');
 
-        const errorHandler = (error: Error): void => logger.log({
-            message: extractErrorMessage(error),
-            level: LogLevel.ERROR,
-        });
+        const loader = jest.fn().mockRejectedValue(error);
+
+        const errorHandler = jest.fn();
 
         const cache = new DefaultWhileMissCache({
-            cacheProvider: inner,
+            provider: provider,
             defaultValue: 'defaultValue',
             errorHandler: errorHandler,
         });
 
         await expect(cache.get('key', loader)).resolves.toBe('defaultValue');
 
-        expect(inner.get).toHaveBeenCalledWith('key', expect.any(Function));
+        expect(provider.get).toHaveBeenCalledWith('key', expect.any(Function));
 
         expect(loader).toHaveBeenCalledWith('key');
 
-        expect(logger.getLogs()).toStrictEqual([{
-            message: 'Some error.',
-            level: LogLevel.ERROR,
-        }]);
+        expect(errorHandler).toHaveBeenCalledTimes(1);
+        expect(errorHandler).toHaveBeenCalledWith(error);
     });
 
-    it('should forward the setting of values', async () => {
-        const inner = new NoopCache();
+    it('should delegate setting a value to the underlying provider', async () => {
+        const provider = new NoopCache();
 
-        jest.spyOn(inner, 'set');
+        jest.spyOn(provider, 'set');
 
         const cache = new DefaultWhileMissCache({
-            cacheProvider: inner,
+            provider: provider,
             defaultValue: 'defaultValue',
         });
 
         await cache.set('key', 'value');
 
-        expect(inner.set).toHaveBeenCalledWith('key', 'value');
+        expect(provider.set).toHaveBeenCalledWith('key', 'value');
     });
 
-    it('should forward deletions', async () => {
-        const inner = new NoopCache();
+    it('should delegate deleting a value to the underlying provider', async () => {
+        const provider = new NoopCache();
 
-        jest.spyOn(inner, 'delete');
+        jest.spyOn(provider, 'delete');
 
         const cache = new DefaultWhileMissCache({
-            cacheProvider: inner,
+            provider: provider,
             defaultValue: 'defaultValue',
         });
 
         await cache.delete('key');
 
-        expect(inner.delete).toHaveBeenCalledWith('key');
+        expect(provider.delete).toHaveBeenCalledWith('key');
     });
 });
